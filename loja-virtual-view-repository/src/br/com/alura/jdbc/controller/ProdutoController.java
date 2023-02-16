6aa50beadb91b8abb2a55d9015b2a98034af482c
josianeemely13 <106481302+josianeemely13@users.noreply.github.com>
2023-02-16 13:18:59 -0300

    Add files via upload

---
+++ loja-virtual-view-repository/src/br/com/alura/jdbc/controller/ProdutoController.java
@@ -0,0 +1,42 @@
+package br.com.alura.jdbc.controller;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+import br.com.alura.jdbc.dao.ProdutoDAO;
+import br.com.alura.jdbc.factory.ConnectionFactory;
+import br.com.alura.jdbc.modelo.Produto;
+
+public class ProdutoController {
+	
+	private ProdutoDAO produtoDAO;
+	
+	public ProdutoController() {
+		Connection connection = new ConnectionFactory().recuperarConexao();
+		this.produtoDAO = new ProdutoDAO(connection);
+	}
+
+	public void deletar(Integer id) {
+		this.produtoDAO.deletar(id);
+		
+	}
+
+	public void salvar(Produto produto) {
+		this.produtoDAO.salvar(produto);
+	}
+
+	public List<Produto> listar() {
+		List<Produto> produtos = 
+				new ArrayList<Produto>();
+		produtos.add(new Produto("Nome do Produto de teste"
+				, "Descrição do produto de teste"));
+		return this.produtoDAO.listar();
+	}
+
+	public void alterar(String nome, String descricao, Integer id) {
+		this.produtoDAO.alterar(nome, descricao, id);
+	}
+}
